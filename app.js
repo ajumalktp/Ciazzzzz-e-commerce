5aa6fc7102060c94e2e1f6e8e9a3545a8a4bf631
ajumal <ajumalktp555@gmail.com>
2023-07-27 11:42:54 +0530

    mdB and ejs updated

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const PORT = process.env.PORT || 8080
 
+
 app.set("view engine","ejs")
-app.get('/',(req,res)=>{
-    res.render('shop')
+app.use(express.static(path.join(__dirname, "public")));
+app.get('/login',(req,res)=>{
+    res.render('userLogin')
+})
+app.get('/signUp',(req,res)=>{
+    res.render('userSignUp')
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Server started at http://localhost:${PORT}`);
-})
+app.listen(PORT,()=>console.log(`Server started at http://localhost:${PORT}`))

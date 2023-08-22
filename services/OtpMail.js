b84313f1db1f9c6e0d522ee37a504435b6707bb0
ajumal <ajumalktp555@gmail.com>
2023-08-22 14:27:28 +0530

    nodemailer otpMail setted

--- services/OtpMail.js
+++ services/OtpMail.js
@@ -1,1 +1,37 @@
 const nodemailer = require('nodemailer')
+
+const sendMail = (name , email , otp)=>{
+    let transporter = nodemailer.createTransport({
+        host:"smtp.gmail.com",
+        port:465,
+        secure:true,
+
+        tls:{
+            rejectUnauthorized:false
+        },
+        auth:{
+            user:process.env.OTP_EMAIL,
+            pass:process.env.OTP_PASS,
+        }
+    })
+    var mailOptions = {
+        from:process.env.OTP_EMAIL,
+        to: email,
+        subject: " Email verification",
+        html: `
+                <h1>Verify Your Email For ShoeFit store</h1>
+                  <h3>${name}please use this code to verify your email</h3>
+                  <h2>${otp}</h2>
+                `,
+      }
+
+      transporter.sendMail(mailOptions,(error,info)=>{
+        if(error){
+            console.log(error)
+        }else{
+            console.log("email sended succesfully");
+        }
+      })
+}
+
+module.exports = sendMail

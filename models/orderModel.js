3184afd7b73a3382fe107c960e888906566dd91e
ajumal <ajumalktp555@gmail.com>
2023-12-07 02:15:18 +0530

    all order page doing

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,35 +1,49 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'users',
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
     },
-    products:[{
-        product:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:'products'
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "products",
         },
-        quantity:{
-            type:Number,
+        quantity: {
+          type: Number,
         },
-        price:{
-            type:Number,
+        price: {
+          type: Number,
         },
-    }],
-    totalAmount:Number,
-    deliveryAddress:Object,
-    paymentMethod:String,
-    status:{
-        type:String,
-        default:'Not Proccesed'
+      },
+    ],
+    totalAmount: Number,
+    deliveryAddress: Object,
+    paymentMethod: String,
+    status: {
+      type: String,
+      default: "Pending",
+
+      enum: [
+        "Pending",
+        "Processing",
+        "Dispatched",
+        "Shipped",
+        "Delivered",
+        "Cancelled",
+        "Returned",
+      ],
     },
-    date:Date,
-},{
-    timestamps:true
-}
-)
+    date: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const orderModel = mongoose.model('orders',orderSchema)
+const orderModel = mongoose.model("orders", orderSchema);
 
-module.exports = orderModel
+module.exports = orderModel;

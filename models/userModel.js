fd666ba9e51788f9ce6227df773787e9a3ba3508
ajumal <ajumalktp555@gmail.com>
2024-03-23 14:12:17 +0530

    admin dashboard finished

--- models/userModel.js
+++ models/userModel.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema({
         type:Boolean,
         default:false
     },
+    wallet:{
+        type:Number,
+        default:0
+    },
     password:String||Number,
 })
 
